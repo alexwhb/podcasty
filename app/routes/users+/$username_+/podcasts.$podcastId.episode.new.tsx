1b1d97c301264d849a0853fb720b4973144798b8
Alex Black <alexwhb@gmail.com>
2025-02-24 18:57:11 -0800

    Updates our podcasts.$podcastId.episode.new.tsx

--- app/routes/users+/$username_+/podcasts.$podcastId.episode.new.tsx
+++ app/routes/users+/$username_+/podcasts.$podcastId.episode.new.tsx
@@ -1,24 +1,17 @@
 import { requireUserId } from '#app/utils/auth.server.ts'
-import { prisma } from '#app/utils/db.server.ts'
-import { Route } from '../../podcast+/+types/$id.episodes.$episodeId.edit'
 
-export async function loader({ params, request }: Route.LoaderArgs) {
-	const userId = await requireUserId(request)
-	const podcast = await prisma.podcast.findUnique({
-		where: { id: params.podcastId, ownerId: userId },
-	})
+import { Route } from './+types/podcasts.$podcastId.episode.new'
+import EpisodeEditor from './__episode-editor'
 
-	if (!podcast) {
-		throw new Response('Podcast not found', { status: 404 })
-	}
+export async function loader({  request }: Route.LoaderArgs) {
+	await requireUserId(request)
 
-	return { podcast }
+	return {  }
 }
 
-export default function NewEpisodeForm() {
-	return (
-		<h1 className="mt-16 w-full text-center text-4xl font-bold">
-			New Episode Form
-		</h1>
-	)
+export default function EditEpisode({
+	actionData,
+}: Route.ComponentProps) {
+	return <EpisodeEditor  actionData={actionData} />
 }
+
